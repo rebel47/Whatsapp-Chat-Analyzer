370914885eb16a8111400098fd4365dabdd61b47
Mohammad Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2025-01-08 09:51:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.sidebar.title("Whatsapp Chat Analyzer")
+st.text("       - Developed By: Mohammad Ayaz Alam")
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
